e58a388b1fac3abf825eea84edd1bf34d45ddd39
samyak <samyakagarwal20@gmail.com>
2023-07-25 17:33:06 +0530

    added changes for DB config

---
+++ src/main/java/com/yflash/SampleJDBC/SampleJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.yflash.SampleJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJdbcApplication.class, args);
+	}
+
+}
